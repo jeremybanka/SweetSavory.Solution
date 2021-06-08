71bf237d0aa77a61d772fca8a34b705e4ada4d42
Jeremy Banka <hello@jeremybanka.com>
2021-06-08 01:39:28 -0700

    🗃️ make id in constructor

--- SweetSavory/Models/Flavoring.cs
+++ SweetSavory/Models/Flavoring.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace SweetSavory.Models
 {
   public class Flavoring
   {
+    public Flavoring()
+    {
+      FlavoringId = Guid.NewGuid().ToString();
+    }
     public string FlavoringId { get; set; }
     public string FlavorId { get; set; }
     public string TreatId { get; set; }
